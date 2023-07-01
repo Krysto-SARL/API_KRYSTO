26b0e1c5044f24e56fdf2e37f1a24afca393944b
Krysto <krysto.contact@gmail.com>
2023-07-01 22:27:33 +1100

    garbage type populate

--- controllers/voluntaryDropPoint.js
+++ controllers/voluntaryDropPoint.js
@@ -1,45 +1,45 @@
-const path = require('path');
-const ErrorResponse = require('../utils/errorResponse');
-const asyncHandler = require('../middlewares/async');
-const geocoder = require('../utils/geocoder');
+const path = require('path')
+const ErrorResponse = require('../utils/errorResponse')
+const asyncHandler = require('../middlewares/async')
+const geocoder = require('../utils/geocoder')
 
-const VoluntaryDropPoint = require('../models/VoluntaryDropPoint');
+const VoluntaryDropPoint = require('../models/VoluntaryDropPoint')
 
 //@description:     Get all voluntary drop points
 //@route:           GET /krysto/api/v2/voluntaryDropPoints
 //@access:          Public
 exports.getVoluntaryDropPoints = asyncHandler(async (req, res, next) => {
-    res.status(200).json(res.advancedResults)
-  });
-
-
+  res.status(200).json(res.advancedResults)
+})
 
 //@description:     Get a single voluntary drop point
 //@route:           GET /krysto/api/v2/voluntaryDropPoints/:id
 //@access:          Public
 exports.getVoluntaryDropPoint = asyncHandler(async (req, res, next) => {
-  const voluntaryDropPoint = await VoluntaryDropPoint.findById(req.params.id);
+  const voluntaryDropPoint = await VoluntaryDropPoint.findById(
+    req.params.id,
+  ).populate('garbageTypes')
   if (!voluntaryDropPoint) {
     return next(
       new ErrorResponse(
         `Voluntary drop point not found with ID of ${req.params.id}`,
-        404
-      )
-    );
+        404,
+      ),
+    )
   }
-  res.status(200).json({ success: true, data: voluntaryDropPoint });
-});
+  res.status(200).json({ success: true, data: voluntaryDropPoint })
+})
 
 //@description:     Create a voluntary drop point
 //@route:           POST /krysto/api/v2/voluntaryDropPoints
 //@access:          Private
 exports.createVoluntaryDropPoint = asyncHandler(async (req, res, next) => {
-  const voluntaryDropPoint = await VoluntaryDropPoint.create(req.body);
+  const voluntaryDropPoint = await VoluntaryDropPoint.create(req.body)
   res.status(201).json({
     success: true,
     data: voluntaryDropPoint,
-  });
-});
+  })
+})
 
 //@description:     Update a voluntary drop point
 //@route:           PUT /krysto/api/v2/voluntaryDropPoints/:id
@@ -51,61 +51,61 @@ exports.updateVoluntaryDropPoint = asyncHandler(async (req, res, next) => {
     {
       new: true,
       runValidators: true,
-    }
-  );
+    },
+  )
 
   res.status(200).json({
     success: true,
     data: voluntaryDropPoint,
-  });
-});
+  })
+})
 
 //@description:     Delete a voluntary drop point
 //@route:           DELETE /krysto/api/v2/voluntaryDropPoints/:id
 //@access:          Private
 exports.deleteVoluntaryDropPoint = asyncHandler(async (req, res, next) => {
   const voluntaryDropPoint = await VoluntaryDropPoint.findByIdAndDelete(
-    req.params.id
-  );
+    req.params.id,
+  )
 
   if (!voluntaryDropPoint) {
     return next(
       new ErrorResponse(
         `Voluntary drop point not found with ID of ${req.params.id}`,
-        404
-      )
-    );
+        404,
+      ),
+    )
   }
 
   res.status(200).json({
     success: true,
     data: {},
-  });
-});
-
+  })
+})
 
 //@description:     Get voluntary drop points within a radius
 //@route:           GET /krysto/api/v2/voluntaryDropPoints/radius/:zipcode/:distance
 //@access:          Private
 
-exports.getVoluntaryDropPointsInRadius = asyncHandler(async (req, res, next) => {
+exports.getVoluntaryDropPointsInRadius = asyncHandler(
+  async (req, res, next) => {
     const { zipcode, distance } = req.params
-  
+
     // Get lat/lng from geocoder
     const loc = await geocoder.geocode(zipcode)
     const lat = loc[0].latitude
     const lng = loc[0].longitude
-  
-    
+
     const radius = distance / 6378
-  
+
     const voluntaryDropPoints = await VoluntaryDropPoint.find({
       location: { $geoWithin: { $centerSphere: [[lng, lat], radius] } },
     })
-  
+
     res.status(200).json({
       success: true,
       count: voluntaryDropPoints.length,
       data: voluntaryDropPoints,
     })
-  })
+  },
+)
