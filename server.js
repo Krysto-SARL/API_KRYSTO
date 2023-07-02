1bbd4727b344edb58739dc6e5565c76c0ad0ae75
Krysto <krysto.contact@gmail.com>
2023-07-02 13:30:29 +1100

    nutriscore in product

--- server.js
+++ server.js
@@ -60,25 +60,25 @@ if (process.env.NODE_ENV === 'development') {
 // File uploading
 app.use(fileupload())
 
-// ======================= Security ====================
-// Sanitize data
-app.use(mongoSanitize())
+// // ======================= Security ====================
+// // Sanitize data
+// app.use(mongoSanitize())
 
-// Set security headers
-app.use(helmet())
+// // Set security headers
+// app.use(helmet())
 
-// Prevent XSS attacks
-app.use(xss())
+// // Prevent XSS attacks
+// app.use(xss())
 
-// Rate limiting
-const limiter = rateLimit({
-  windowMs: 10 * 60 * 1000, // 10 mins
-  max: 100,
-})
-app.use(limiter)
+// // Rate limiting
+// const limiter = rateLimit({
+//   windowMs: 10 * 60 * 1000, // 10 mins
+//   max: 100,
+// })
+// app.use(limiter)
 
-// Prevent http param pollution
-app.use(hpp())
+// // Prevent http param pollution
+// app.use(hpp())
 
 // Enable CORS
 app.use(
